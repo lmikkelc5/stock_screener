03b8d2e20839bff330392a81c63bf49841b2c8ff
Mikkel <lmikkelc5@gmail.com>
2025-10-03 12:06:37 -0600

    add basic layout, loop through list of stocks and create a dataframe with specified data

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from apikey import api_key
 import screen
+import pandas as pd
 
 def main():
-    print(screen.screener('PRCH', api_key))
+    ticks = pd.read_csv('data/SP500.csv')['Symbol'].tolist()
+    new_df = pd.DataFrame()
 
+    for item in ticks:
+        new_df = pd.concat([new_df, screen.screener('AAPL', api_key)], ignore_index=True)
 
+    print(new_df)
+        
 if __name__ == "__main__":
     main()
--- screen.py
+++ screen.py
@@ -1,15 +1,29 @@
 import requests
+import yfinance as yf
+import pandas as pd
 
 
-
-def screener_stock(ticker, api_key):
+def screener(ticker, api_key):
     #use response.get() to get the data
-    url = f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={ticker}&apikey={api_key}"
-    response = requests.get(url)
+
+    #alphavantage
+    # url = f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={ticker}&apikey={api_key}"
+    # response = requests.get(url)
+
+    #yfinance
+    data = yf.Ticker(ticker)
+
 
     #figure out what form the data comes in/needs to be in
+    row = {
+        'ticker' : [ticker],
+        'sector' : [data.info['sector']],
+        'stock_price' : [data.history(period = '1d')["Close"].iloc[-1]],
+        'pe_ratio' : [data.info['trailingPE']]
+    }
 
-    #run data through the screen    
+    #run data through the screen  
+    df = pd.DataFrame(row)  
 
     #return the screened data
-    return url
+    return df
