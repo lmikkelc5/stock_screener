d89ef8b504e644d741a1292d1e29de7493d4d5d8
Mikkel <lmikkelc5@gmail.com>
2025-10-03 19:05:45 -0600

    add the structure and basic functionality

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from apikey import api_key
 import screen
 import pandas as pd
+import warnings
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 def main():
-    ticks = pd.read_csv('data/SP500.csv')['Symbol'].tolist()
-    new_df = pd.DataFrame()
+    #read in list 
+    run_check = input('Do you want to run the screener(Y or N): ')
 
-    for item in ticks:
-        new_df = pd.concat([new_df, screen.screener('AAPL', api_key)], ignore_index=True)
+    with open('data/cleaned_list.txt', 'r') as f:
+        ticks = [line.strip() for line in f.readlines()]
 
-    print(new_df)
+    if run_check.upper() == 'Y':
+        with open('data/cleaned_list.txt', 'w') as f:
+            cleaned_list, invalid_list = screen.check_list(ticks)
+            for sym in cleaned_list:
+                f.write(sym + '\n')
+
+        with open('data/invalid_list.txt', 'w') as f:
+            for sym in invalid_list:
+                f.write(sym + '\n')
+
+    # if yes use clened_list if no use ticks
+    if run_check.upper() == 'Y':
+        lst = cleaned_list
+    else:
+        lst = ticks
+
+    #create new df
+    main_df = pd.DataFrame()
+
+    for item in lst:
+        new_row = screen.screener(item, api_key)
+        main_df = pd.concat([main_df, new_row], ignore_index=True)
+
+    print(main_df)
         
 if __name__ == "__main__":
     main()
--- screen.py
+++ screen.py
@@ -2,28 +2,64 @@ import requests
 import yfinance as yf
 import pandas as pd
 
+def check_list(ticker_lst, verbose = True):
+    valid = []
+    invalid = []
+
+    for sym in ticker_lst:
+        try:
+            t = yf.Ticker(sym)
+            hist = t.history(period = '1d')
+
+            if hist.empty:
+                invalid.append(sym)
+                if verbose:
+                    print(f"{sym}: Possibly delisted")
+            else:
+                valid.append(sym)
+                if verbose:
+                    print(f"{sym}: Valid")
+        except Exception as e:
+            invalid.append(sym)
+            if verbose:
+                print(f"{sym}: Error fetching ({e})")
+
+    if verbose:
+        print("\nSummary")
+        print(f"Valid tickers: {len(valid)}")
+        print(f"Invalid Tickers: {len(valid)}")
+
+    return valid, invalid
 
 def screener(ticker, api_key):
-    #use response.get() to get the data
-
     #alphavantage
     # url = f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={ticker}&apikey={api_key}"
     # response = requests.get(url)
 
     #yfinance
     data = yf.Ticker(ticker)
+    hist = data.history(period = '1d')
 
+    #get data
+    try:
+        stock_price = data.history(period = '1d')["Close"].iloc[-1]
+    except Exception:
+        stock_price = None
 
-    #figure out what form the data comes in/needs to be in
+    sector = data.info.get('sector', None)
+    trailing_PE = data.info.get('trailingPE', None)
+
+    #make the df
     row = {
         'ticker' : [ticker],
-        'sector' : [data.info['sector']],
-        'stock_price' : [data.history(period = '1d')["Close"].iloc[-1]],
-        'pe_ratio' : [data.info['trailingPE']]
+        'sector' : [sector],
+        'stock_price' : [stock_price],
+        'pe_ratio' : [trailing_PE]
     }
 
-    #run data through the screen  
-    df = pd.DataFrame(row)  
+    #create df  
+    df = pd.DataFrame(row) 
 
-    #return the screened data
+
+    #return the data
     return df
